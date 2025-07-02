e0edd2c1e1df73f3dc45142041ac0f683ecc92c4
Yuliia Hudyma <103529155+gudymayuliia@users.noreply.github.com>
2025-07-02 14:21:20 +0300

    Simple color test

--- top.sv
+++ top.sv
@@ -2,15 +2,39 @@
 
 
 
-module top (output logic led_red, output logic led_green);
+module top (input logic [1:0] sw, 
+            output logic led0_r, 
+            output logic led0_g,
+            output logic led0_b,
+            output logic led1_r,
+            output logic led1_g,
+            output logic led1_b);
+    
 
-    // Hardcoded 8-bit signals
-    logic [7:0] a = 8'b00000001;
-    logic [7:0] b = 8'b00000010;
+    //logic [7:0] a = 8'b00000000;
+    //logic [7:0] b = 8'b00000000;
 
-    // Connect LSBs to LEDs
-    assign led0_g   = a[0];
-    assign led1_r = b[0];
+    always_comb begin
+        if(sw[0]) begin
+            led0_b = 1;
+            led0_g = 1;
+            led0_r = 0;
+        end else begin
+            led0_b = 0;
+            led0_g = 0;
+            led0_r = 0;
+        end
+
+        if(sw[1]) begin
+            led1_b = 0;
+            led1_g = 0;
+            led1_r = 1;
+        end else begin
+            led1_b = 0;
+            led1_g = 0;
+            led1_r = 0;
+        end
+    end
 
 endmodule
 
