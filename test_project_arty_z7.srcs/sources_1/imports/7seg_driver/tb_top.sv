a3d6ad1967ca831dbe936ddeb415220020874a24
Yuliia Hudyma <gudyma.yullia@gmail.com>
2025-07-27 22:54:30 +0300

    Initial commit for project with 7 segment display

---
+++ test_project_arty_z7.srcs/sources_1/imports/7seg_driver/tb_top.sv
@@ -0,0 +1,49 @@
+module tb_top();
+
+logic clk, porb, sync_reset;
+tri1 seg_sda, seg_scl;
+logic sda_in;
+
+initial begin
+    porb = 0;
+    sync_reset = 1;
+    #120ns;
+    @(posedge clk);
+    porb = 1;
+    repeat(20) @(posedge clk);
+    sync_reset = 0;
+
+    #200ms;
+    $finish();
+
+end
+
+initial begin
+    //seg_sda = 1;
+    forever begin
+        repeat(9) @(negedge seg_scl);
+        #10ns;
+        //seg_sda = 0;
+        @(negedge seg_scl)
+        #10ns;
+        //seg_sda = 1;
+    end
+end
+
+
+initial begin
+    clk = 0;
+    forever #40ns clk = ~clk;
+end
+
+
+top top_u(
+    .clk_i(clk),
+    .porb_i(porb),
+    .sync_reset_i(sync_reset),
+    .sda(seg_sda),
+    .scl(seg_scl)
+);
+
+
+endmodule
---
+++ test_project_arty_z7.srcs/sources_1/imports/7seg_driver/top.sv
@@ -0,0 +1,115 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/09/2025 06:01:36 PM
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+`include "driver.svh"
+
+
+module top(
+    input  clk_i,
+    input  porb_i,
+    input  sync_reset_i,
+    inout  sda,
+    output scl
+);
+
+wire sda_in, seg_scl, sda_out_en, sda_out;
+
+typedef enum logic [1:0] {
+    IDLE,
+    SEND_NUMBER,
+    WAIT,
+    DONE
+} state_t;
+
+state_t fsm_state_ff, fsm_next;
+logic [7:0] digits [3:0];
+logic disp_strobe;
+
+// FSM state FF
+always_ff @(negedge porb_i, posedge clk_i ) begin : state_ff
+    if(!porb_i)begin
+    fsm_state_ff <= IDLE;
+    end else if (clk_i) begin
+        if(sync_reset_i) begin
+            fsm_state_ff <= IDLE;
+        end else begin
+            fsm_state_ff <= fsm_next;
+        end
+    end 
+end
+
+// State transitioning logic
+always_comb begin : next_state
+    fsm_next = fsm_state_ff;
+    disp_strobe = 1'b0;
+    digits[0] = 8'h00;
+    digits[1] = 8'h00;
+    digits[2] = 8'h00;
+    digits[3] = 8'h00;
+    case (fsm_state_ff)
+        IDLE: 
+            begin
+                fsm_next = SEND_NUMBER;
+            end
+        SEND_NUMBER:
+            begin
+                disp_strobe = 1'b1;
+                digits[0] = `_2;
+                digits[1] = `_0;
+                digits[2] = `_2;
+                digits[3] = `_5;
+                fsm_next = WAIT;
+            end
+        WAIT:
+            begin
+                if (!busy) begin
+                    fsm_next = DONE;
+                end
+            end
+        DONE:
+            fsm_next = fsm_state_ff;
+        default: fsm_next = fsm_state_ff;
+    endcase
+end
+
+
+driver #(
+    .CLK_DIV(1024)
+)
+driver_u
+(
+    .clk_i(clk_i),
+    .porb_i(porb_i),
+    .sync_reset_i(sync_reset_i),
+    .digits_i(digits),
+    .disp_strobe_i(disp_strobe),
+    .busy_o(busy),
+    .sda_out(sda_out),
+    .sda_in(sda_in),
+    .sda_out_en(sda_out_en),
+    .seg_scl_o(seg_scl)
+);
+
+assign sda_in = sda;
+assign sda = (!sda_out_en || sda_out) ? 'Z : '0;
+
+assign scl = seg_scl;// ? 'Z : '0;
+
+endmodule
