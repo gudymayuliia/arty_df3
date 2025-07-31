7eda1f67c1c3da5b896c67aa008ec2fdd35dc052
Yuliia Hudyma <gudyma.yullia@gmail.com>
2025-07-31 17:42:59 +0300

    Working version of 7 segment display project. Added two counters which are controlled by buttons and switch

--- test_project_arty_z7.srcs/sources_1/imports/7seg_driver/seven_segment_top.sv
+++ test_project_arty_z7.srcs/sources_1/imports/7seg_driver/seven_segment_top.sv
@@ -15,20 +15,32 @@ module seven_segment_top(
 
 wire sda_in, seg_scl, sda_out_en, sda_out;
 
-typedef enum logic [1:0] {
-    IDLE,
-    SEND_NUMBER,
-    WAIT,
-    DONE
-} state_t;
 
-state_t fsm_state_ff, fsm_next;
 logic [7:0] digits [3:0];
 logic disp_strobe;
 
+function automatic logic [7:0] get_digit_pattern(input int number);
+    case (number)
+        0: return `_0;
+        1: return `_1;
+        2: return `_2;
+        3: return `_3;
+        4: return `_4;
+        5: return `_5;
+        6: return `_6;
+        7: return `_7;
+        8: return `_8;
+        9: return `_9;
+        default: return 8'h00;
+    endcase
+endfunction
+
+
 //sw1 - change counter
 //sw0 - reset
-logic [7:0] counter_a, counter_b, selected_counter;
+logic [7:0] counter_a;
+logic [7:0] counter_b; 
+logic [7:0] selected_counter;
 logic [3:0] btn_db;
 logic btn_db_prev0, btn_db_prev1, btn_db_prev2, btn_db_prev3;
 
@@ -43,89 +55,60 @@ button_debounce b3 (.clk(clk_i), .in(btn[3]), .out(btn_db[3]));
 
 
 always_ff @(posedge clk_i or negedge porb_i) begin
-    if (!porb_i)
+    if (porb_i == 0)
         selected_counter <= 0;
     else
         selected_counter <= (sw == 1'b0) ? counter_a : counter_b;
 end
 
-
-
-
 always_ff @(negedge porb_i, posedge clk_i) begin
-    if (!porb_i) begin
+    if (porb_i == 0) begin//!porb_i
         counter_a <= 8'd0;
         counter_b <= 8'd0;
-    end else if (clk_i) begin
+    end else  begin
         btn_db_prev0 <= btn_db[0];
         btn_db_prev1 <= btn_db[1];
         btn_db_prev2 <= btn_db[2];
         btn_db_prev3 <= btn_db[3];
         
-//        if (btn_db[0] && !btn_db_prev0) counter_a <= counter_a + 1;
-//        if (btn_db[1] && !btn_db_prev1) counter_a <= counter_a - 1;
-//        if (btn_db[2] && !btn_db_prev2) counter_b <= counter_b + 1;
-//        if (btn_db[3] && !btn_db_prev3) counter_b <= counter_b - 1;
+        if (btn_db[0] && !btn_db_prev0) begin 
+            counter_a <= counter_a + 1;
+        end
+        if (btn_db[1] && !btn_db_prev1) begin 
+            if(counter_a > 0) begin
+                counter_a <= counter_a - 1;
+            end else 
+                counter_a <= 8'd0;
+        end
+        if (btn_db[2] && !btn_db_prev2) begin
+            counter_b <= counter_b + 1;
+        end
+        if (btn_db[3] && !btn_db_prev3) begin 
+            if(counter_b > 0) begin
+                counter_b <= counter_b - 1;
+            end else 
+                counter_b <= 8'd0;
+        end
 
-        if (btn[0]) counter_a <= counter_a + 1;
-        if (btn[1]) counter_a <= counter_a - 1;
-        if (btn[2]) counter_b <= counter_b + 1;
-        if (btn[3]) counter_b <= counter_b - 1;
     end
 end
 
-// FSM state FF
-always_ff @(negedge porb_i, posedge clk_i ) begin : state_ff
-    if(!porb_i)begin
-    fsm_state_ff <= IDLE;
-    end else if (clk_i) begin
-        fsm_state_ff <= fsm_next;
-    end 
+always_comb begin
+    digits[0] = `_0;
+    digits[1] = `_0;
+    digits[2] = `_0;
+    digits[3] = `_0;
+
+
+    digits[0] = get_digit_pattern(selected_counter % 10);    
+    digits[1] = get_digit_pattern((selected_counter / 10) % 10);     
+    digits[2] = get_digit_pattern((selected_counter / 100) % 10);     
+    digits[3] = get_digit_pattern((selected_counter / 1000) % 10);  
+                                
+
+    disp_strobe = 1'b1;
 end
 
-// State transitioning logic
-always_comb begin : next_state
-    fsm_next = fsm_state_ff;
-    disp_strobe = 1'b0;
-    digits[0] = 8'h00;
-    digits[1] = 8'h00;
-    digits[2] = 8'h00;
-    digits[3] = 8'h00;
-    case (fsm_state_ff)
-        IDLE: 
-            begin
-                fsm_next = SEND_NUMBER;
-            end
-        SEND_NUMBER:
-            begin
-                disp_strobe = 1'b1;
-                digits[3] = `_5;
-
-                
-                if(selected_counter > 2)  digits[0] = `_2;
-                if(selected_counter > 7)  digits[1] = `_7;
-                if(selected_counter > 10)  digits[2] = `_8;
-                if(selected_counter > 13)  digits[3] = `_9;
-
-                                                       
-//                digits[0] = `_2;
-//                digits[1] = `_0;
-//                digits[2] = `_2;
-//                digits[3] = `_5;
-                
-                fsm_next = WAIT;
-            end
-        WAIT:
-            begin
-                if (!busy) begin
-                    fsm_next = DONE;
-                end
-            end
-        DONE:
-            fsm_next = fsm_state_ff;
-        default: fsm_next = fsm_state_ff;
-    endcase
-end
 
 
 driver #(
@@ -149,5 +132,17 @@ assign sda_in = sda;
 assign sda = (!sda_out_en || sda_out) ? 'Z : '0;
 
 assign scl = seg_scl;// ? 'Z : '0;
+
+ila_0 your_instance_name (
+	.clk(clk_i), // input wire clk
+
+
+	.probe0(counter_a), // input wire [7:0]  probe0  
+	.probe1(counter_b), // input wire [7:0]  probe1 
+	.probe2(btn), // input wire [3:0]  probe2 
+	.probe3(sw), // input wire [0:0]  probe3 
+	.probe4(porb_i), // input wire [0:0]  probe4 
+	.probe5(btn_db) // input wire [3:0]  probe5
+);
 
 endmodule
